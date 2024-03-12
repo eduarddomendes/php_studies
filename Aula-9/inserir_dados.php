f8e4abeee32f4903f90d759cbaf39966a2493a35
eduarddomendes <emdbm.10@gmail.com>
2024-03-11 22:03:54 -0300

    add class

---
+++ Aula-9/inserir_dados.php
@@ -0,0 +1,11 @@
+<?php
+
+echo $_GET["nome"];
+$pdo = new PDO("sqlite:estoque.db");
+
+$queryInsereCategoria = "INSERT INTO categorias (nome)
+VALUES (\"".$_GET["nome"]."\")";
+
+ $pdo->query($queryInsereCategoria);
+
+?>
